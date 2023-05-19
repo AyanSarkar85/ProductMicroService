a946e18cf1486862d33cf1d2cbfb7f90aaaee6d9
ayana <ayana@BLACKPEARL>
2023-05-20 00:59:10 +0530

    Add project files.

---
+++ ProductMicroService/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ProductMicroService.Models;
+using ProductMicroService.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductMicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _productRepository.GetProducts();
+            return Ok(products);
+        }
+                
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product=_productRepository.GetProductByID(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            _productRepository.InsertProduct(product);
+            return CreatedAtAction(nameof(Get), product.Id);
+        }
+                
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            if(product == null)
+            {
+                return NoContent();
+            }
+            _productRepository.UpdateProduct(product);
+            return Ok();
+        }
+                
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _productRepository.DeleteProduct(id);
+            return Ok();
+        }
+    }
+}
---
+++ ProductMicroService/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ProductMicroService.DBContexts;
+using ProductMicroService.Models;
+
+namespace ProductMicroService.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                Save();
+            }
+        }
+
+        public Product? GetProductByID(int ProductId)
+        {
+            return _dbContext.Products.Find(ProductId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Products.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {            
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
